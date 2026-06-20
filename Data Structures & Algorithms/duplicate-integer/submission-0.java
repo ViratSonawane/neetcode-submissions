e6e35775618c82ae4a0ef9e4051f8ea1e5b7e94b
ViratSonawane <virat.sonawane23@spit.ac.in>
2026-06-20 09:04:43 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int val : nums){
+            if(set.contains(val)){
+                return true;
+            }
+            set.add(val);
+        }
+
+        return false;
+    }
+}
