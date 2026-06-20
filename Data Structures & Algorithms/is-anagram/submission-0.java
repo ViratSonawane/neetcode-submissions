e92270e1545bab95bb908b49d5ce3b214d8acbb1
ViratSonawane <virat.sonawane23@spit.ac.in>
2026-06-20 09:24:08 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       int[] arr = new int[26];
+        int m = s.length();
+        int n = t.length();
+
+        if(m != n){
+            return false;
+        }
+
+       for(int i = 0; i < m; i++){
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+       } 
+
+       for(int val : arr){
+            if(val != 0){
+                return false;
+            }
+       }
+
+       return true;
+    }
+}
